c6bea7443a6074f92f76c799e0e738326b90397f
fakergh <151444595+fakergh@users.noreply.github.com>
2023-11-20 09:26:57 +0530

    Gradient.py

--- Gradient.py
+++ Gradient.py
@@ -1,12 +1,14 @@
+import numpy as np
+from matplotlib import pyplot
+
+def function_to_minimize(x):
+    return (x + 3)**2
+
 def gradient_descent(starting_point, learning_rate, num_iterations):
     x = starting_point
-
-    def function_to_minimize(x):
-        return (x + 3)**2
-
+    
     for _ in range(num_iterations):
         gradient = 2 * (x + 3)  # Derivative of the function
-
         x = x - learning_rate * gradient
 
     return x, function_to_minimize(x)
@@ -19,3 +21,9 @@ minimum_point, minimum_value = gradient_descent(initial_point, learning_rate, it
 
 print("Local minimum point:", minimum_point)
 print("Minimum value of the function:", minimum_value)
+
+
+x_cordinate = np.linspace(-15,15,100)
+pyplot.plot(x_cordinate,function_to_minimize(x_cordinate))
+pyplot.plot(-3,function_to_minimize(-3),'ro')
+pyplot.show()
